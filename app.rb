de964035f252e9c68aeaea78b1331986b1c7aa88
Adel MALIK-MONROCQ <dedeloum@icloud.com>
2020-04-21 16:01:29 +0200

    Exo1

--- app.rb
+++ app.rb
@@ -4,4 +4,20 @@ Bundler.require
 require_relative 'lib/game'
 require_relative 'lib/player'
 
+def perform
+  player1 = Player.new("Josiane")
+  player2 = Player.new("José")
+  while player1.life_points > 0 && player2.life_points > 0
+    p "Voici l'état de chaque joueur :"
+    player1.show_state
+    player2.show_state
+    p "Passons à la phase d'attaque"
+    player1.attacks(player2)
+    break if player2.life_points <= 0
+    player2.attacks(player1)
+  end
+end
+
+perform
+
 binding.pry
--- lib/player.rb
+++ lib/player.rb
@@ -1,3 +1,32 @@
 class Player
-  
+  attr_accessor :name, :life_points
+
+  def initialize(name)
+    @name = name
+    @life_points = 10
+  end
+
+  def show_state
+    p "#{@name} a #{@life_points} points de vie"
+  end
+
+  def gets_damage(points)
+    @life_points -= points
+    if @life_points <= 0
+      p "le joueur #{@name} a été tué !"
+    end
+  end
+
+  def attacks(player)
+    p "le joueur #{@name} attaque le joueur #{player.name}"
+    damage_computed = compute_damage
+    p "il lui inflige #{damage_computed} points de dommages"
+    player.gets_damage(damage_computed)
+  end
+
+  private
+
+  def compute_damage
+    return rand(1..6)
+  end
 end
